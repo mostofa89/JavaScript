6fd7f8c9cc26dbfef28705566b47b4075ca396b9
mostofa89 <mostofakamaljoy89@gmail.com>
2025-10-16 15:30:20 +0600

    Add For Loop

---
+++ Fundamentals/iteration/forLoop/biger_step.js
@@ -0,0 +1,5 @@
+{
+    for (let i = 0; i <= 100; i += 10){
+        console.log("Value of i is now", i);
+    }
+}
---
+++ Fundamentals/iteration/forLoop/for_each.js
@@ -0,0 +1,6 @@
+{
+    let nums = [1, 2, 3, 4, 9, 7, 8, 0, 7]
+    for (let i in nums){ // here i is the index
+        console.log(nums[i]);
+    }
+}
