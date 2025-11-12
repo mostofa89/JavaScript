59caa5a7a765b25d1aee1a59e0dbdcb25c2bba08
mostofa89 <mostofakamaljoy89@gmail.com>
2025-11-12 21:21:37 +0600

    Add Formated String

---
+++ Fundamentals/Formated_str/normalrep.js
@@ -0,0 +1,15 @@
+let text = "Hello, World!";
+let number = 42;
+let floatNum = 3.14159;
+let booleanValue = true;
+let array = [1, 2, 3];
+let object = { key: "value" };
+
+console.log("String: ", text);
+console.log("Integer: ", number);
+console.log("Float: ", floatNum);
+console.log("Boolean: ", booleanValue);
+console.log("Array: ", array);
+console.log("Object: ", object);
+
+console.log("String: ", text , "number: ", number , "float: ", floatNum , "boolean: ", booleanValue , "array: ", array , "object: ", object);
